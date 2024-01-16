b73244c112799401d2dfaa5c231ea8992418ecaa
KikeDeAlba <likingvirus1@gmail.com>
2024-01-16 04:02:56 -0700

    Update version number and add project URL

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='crud-constructor',
-    version='1.1',
+    version='1.0',
     packages=find_packages(),
     install_requires=[
         'mysqlclientpy'
     ],
+    url='https://github.com/KikeDeAlba/crud-constructor'
 )
