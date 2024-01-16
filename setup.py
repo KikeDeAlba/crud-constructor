a483433ec31078afe66f256ee9d863568b5e2768
KikeDeAlba <likingvirus1@gmail.com>
2024-01-16 03:04:00 -0700

    Add setup.py file for package configuration

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='crud-constructor',
+    version='1.1',
+    packages=find_packages(),
+    install_requires=[
+        'fastapi'
+    ],
+)
